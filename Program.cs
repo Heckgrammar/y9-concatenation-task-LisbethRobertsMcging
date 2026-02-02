3dabd48f79a1c550a646d06b81a96d86f95c8b67
Lisbeth Roberts-Mcging <23LRoberts-Mcging@heckgrammar.co.uk>
2026-02-02 09:49:26 +0000

    .

--- Program.cs
+++ Program.cs
@@ -5,16 +5,21 @@
         static void Main(string[] args)
         {
             string FirstName;
+            string LastName;
+            string Age;
             Console.Write("Enter first name: ");
             FirstName = Console.ReadLine();
+            Console.Write("Enter last name: ");
+            LastName = Console.ReadLine();
+            Console.Write("Enter age: ");
+            Age = Console.ReadLine();
             // add two more variables for the users last name and their age
             // take these inputs into the program
             // use the test data Bob, Smith, 14
             // ammend this code to output in one command
             // Hello Bob Smith you are 14 years old
-            Console.WriteLine(FirstName);
+            Console.WriteLine("Hello " + FirstName + " " + LastName + " you are " + Age + " years old.");
             // save and commit your program
-            // add evidence of the test input and your own input running the program 
+            // add evidence of the test input and your own input running the program        }
         }
     }
-}
